fc6c4ec1666f05c8b18ddeb2ac3c97e90433152b
soys <Soys@users.noreply.github.com>
2020-02-24 01:01:09 +0800

    整合合辑至投稿内

--- AcFun/member-center.user.js
+++ AcFun/member-center.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         会员中心投稿修正
 // @namespace    https://github.com/Soys/user.js/tree/master/AcFun
-// @version      0.3
+// @version      0.4
 // @description  修正会员中心投稿相关按钮
 // @author       Soys
 // @match        https://www.acfun.cn/member/
@@ -16,11 +16,12 @@
     var acpart = aclist.getElementsByClassName('part-guide-left');
     acpart[1].getElementsByTagName('div')[1].className = 'mainer';
     acpart[3].className = 'part-guide-left hidden';
+    acpart[6].className = 'part-guide-left hidden';
     var acdiv = acpart[5].getElementsByTagName('div');
     var aclink = acdiv[0].getElementsByTagName('a');
     aclink[0].innerHTML = '<i class="icon icon-upload"></i>创作中心';
     aclink[0].href = '#area=post-history';
     aclink[0].className = 'tab fixed';
-    acpart[5].innerHTML = acpart[5].innerHTML + '<div class="mainer"><a href="#area=upload-video" class="tab"><i class="icon"></i>视频投稿</a><a href="#area=post-article" class="tab"><i class="icon"></i>文章投稿</a><a href="#area=post-history" class="tab"><i class="icon"></i>过往投稿</a><a href="https://member.acfun.cn/upload-video" class="tab"><i class="icon"></i>视频投稿(新)</a><a href="https://member.acfun.cn/post-article" class="tab"><i class="icon"></i>文章投稿(新)</a></div>';
+    acpart[5].innerHTML = acpart[5].innerHTML + '<div class="mainer"><a href="#area=upload-video" class="tab"><i class="icon"></i>视频投稿</a><a href="#area=post-article" class="tab"><i class="icon"></i>文章投稿</a><a href="#area=post-history" class="tab"><i class="icon"></i>过往投稿</a><a href="https://member.acfun.cn/" class="tab"><i class="icon"></i>新投稿中心</a><a href="#area=create-album" class="tab"><i class="icon"></i>创建合辑</a><a href="#area=album-manage" class="tab"><i class="icon"></i>合辑管理</a></div>';
     // Your code here...
 })();
