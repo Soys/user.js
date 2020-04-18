06d203c288066f2056cc12805ba058b76283b245
soys <Soys@users.noreply.github.com>
2020-04-18 17:11:24 +0000

    Update member-center.user.js
    隐藏

--- AcFun/member-center.user.js
+++ AcFun/member-center.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
-// @name         会员中心投稿修正
+// @name         用户中心投稿修正
 // @namespace    https://github.com/Soys/user.js/tree/master/AcFun
-// @version      0.4
-// @description  修正会员中心投稿相关按钮
+// @version      0.5
+// @description  修正用户中心投稿相关按钮
 // @author       Soys
 // @match        https://www.acfun.cn/member/
 // @downloadURL  https://github.com/Soys/user.js/raw/master/AcFun/member-center.user.js
@@ -23,5 +23,5 @@
     aclink[0].href = '#area=post-history';
     aclink[0].className = 'tab fixed';
     acpart[5].innerHTML = acpart[5].innerHTML + '<div class="mainer"><a href="#area=upload-video" class="tab"><i class="icon"></i>视频投稿</a><a href="#area=post-article" class="tab"><i class="icon"></i>文章投稿</a><a href="#area=post-history" class="tab"><i class="icon"></i>过往投稿</a><a href="https://member.acfun.cn/" class="tab"><i class="icon"></i>新投稿中心</a><a href="#area=create-album" class="tab"><i class="icon"></i>创建合辑</a><a href="#area=album-manage" class="tab"><i class="icon"></i>合辑管理</a></div>';
-    // Your code here...
+    acpart[7].className = acpart[7].className + 'hidden';
 })();
