87955ab98785a8ce37165f1de0244a2d264f651a
soys <Soys@users.noreply.github.com>
2020-07-14 16:22:23 +0800

    Asearch.user.js
    AcFun搜索辅助小工具

---
+++ AcFun/Asearch.user.js
@@ -0,0 +1,38 @@
+// ==UserScript==
+// @name         Asearch
+// @namespace    http://127.0.0.1/
+// @version      0.11
+// @description  AcFun搜索引擎小工具
+// @author       饃鶻荔௰
+// @match        https://www.acfun.cn/search?*
+// @match        https://www.acfun.cn/search
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    function getQueryVariable(variable)
+{
+       var query = window.location.search.substring(1);
+       var vars = query.split("&");
+       for (var i=0;i<vars.length;i++) {
+               var pair = vars[i].split("=");
+               if(pair[0] == variable){return pair[1];}
+       }
+       return(false);
+}
+    var barS = document.getElementById('search-box--standalone');
+    var Sform = barS.getElementsByClassName('form')[0];
+    /*var Stext = Sform.getElementsByTagName('input')[0].value;*/
+    var Stext = getQueryVariable("keyword");
+    var SearchPlus = document.createElement("div");
+        SearchPlus.style = 'text-align:center;';
+    var ViewType = 'view_window';
+    var AcGoogle = 'https://www.google.com/search?q=site:acfun.cn+'+ Stext +'&oq=site';
+    var AcBaidu = 'https://www.baidu.com/s?wd=site:acfun.cn+'+ Stext;
+    var AcBing = 'https://www.bing.com/search?q=site:acfun.cn+'+ Stext;
+    var AcDDG = 'https://duckduckgo.com/?q=site:acfun.cn+'+ Stext;
+    var AcSG = 'https://www.sogou.com/web?query=site:acfun.cn+'+ Stext;
+    SearchPlus.innerHTML = '<a href='+AcBaidu+' target='+ViewType+'>百度搜索</a> | <a href='+AcGoogle+' target='+ViewType+'>谷歌搜索</a> | <a href='+AcBing+' target='+ViewType+'>必应搜索</a> | <a href='+AcDDG+' target='+ViewType+'>DDG搜索</a> | <a href='+AcSG+' target='+ViewType+'>搜狗搜索</a>';
+    Sform.append(SearchPlus);
+    // Your code here...
+})();
